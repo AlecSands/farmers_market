55e2e14d0235b747a4ebe2fabf3a3e67ab8b36f7
Alessandro Leyva <josealeyva87@gmail.com>
2017-08-03 11:26:53 -0500

    before call

--- server/routes/market.router.js
+++ server/routes/market.router.js
@@ -6,17 +6,10 @@
 
 // express module
 var express = require('express');
-<<<<<<< HEAD
-
-// Express router to mount market related functions on.
-var router = express.Router();
-var Users = require('../models/user.js');
-=======
 var user =
   // Express router to mount market related functions on.
   var router = express.Router();
 
->>>>>>> feature-marketPLace-ale
 // For base mode, use this array
 // As a stretch goal, move this to the database
 var marketItems = [{
@@ -78,95 +71,31 @@ var marketItems = [{
  * @param {Object} res - Express response object
  */
 
-<<<<<<< HEAD
-function changeItemCost() {
-
-};
-
-router.get('/items', function(req, res){
-  console.log('marketRouter - get /items');
-  res.send(marketItems);
-});
-
-router.put('/buy/:id', function(req, res){
-  console.log('marketRouter - put /buy');
-
-  // TODO: Save to the database
-  res.sendStatus(200); // <- Temporary
-});
-
-router.put('/sell/:id', function(req, res){
-  console.log('marketRouter - put /sell');
-  // TODO: Save to the database
-  res.sendStatus(200); // <- Temporary
-});
-
-router.get('/leaderboard', function(req, res){
-  console.log('marketRouter - get /leaderboard');
-  // TODO: Retrieve from the database
-
-  Users.find({}, function(err, result) { //find * (same as in mongoose)
-  if(err) {
-    console.log('find error: ', err);
-    res.sendStatus(500);
-  } else {
-    //res.send(data); //array of objects - each obj a document in the collectin in the db
-    //res.send(result.rows) - same as
-
-    var arrayOfResults = [];
-    for(i=0;i<result.length;i++){
-      var eachUserStuff = {};
-       eachUserStuff.username = result[i].username;
-       eachUserStuff.moneys = result[i].money;
-      //var x = result[i].username;
-      //console.log(x);
-      arrayOfResults.push(eachUserStuff);
-    }
-    console.log('got all users ', arrayOfResults);
-    res.send(arrayOfResults);
-  }//end if
-}).sort({money:-1});//end find
-
-  //res.send([]); // <- Temporary
-});
-
-module.exports = router;
-
-
-function chooseRandomPrice() {
-  console.log('chooseRandomPrice');
-  console.log((Math.random() * (0.01 - 0.15) + 0.15).toFixed(2));
-  return (Math.random() * (0.01 - 0.15) + 0.15).toFixed(2);
-}
-
-var productPrice;
-function changePriceOnTimer() {
-  productPrice = setInterval(chooseRandomPrice, 10000);
-}
-
-console.log(changePriceOnTimer());
-=======
 buyItem = function(id) {
-    id = req.params.id;
-    data = req.body;
+    itemId = req.params.id;
+    user = req.user;
+    console.log(req.user);
     var basket = [];
     console.log('in update:', id);
     var price = marketItems[id - 1].cost;
     if (req.user.money >= price) {
-      user.findByIdAndUpdate({
-          _id: id
-        }, {
-          $set: {
-            basket: data.basket,
-            money: data.money
+      User.findByIdAndUpdate(
+        {_id: req.user.id},
+        { $set: {
+            basket: user.basket,
+            money: user.money
+          }},
+          function(err, data) {
+               if(err) {
+                 console.log('remove error: ', err);
+                 res.sendStatus(500);
+               } else {
+                 res.sendStatus(200);
+               }
+             }
+           );
           }
-        },
-      }
-      else {
-        res.send('buy stuff');
-      }
-    }
-
+        };
 
 
 
@@ -195,4 +124,3 @@ buyItem = function(id) {
     });
 
     module.exports = router;
->>>>>>> feature-marketPLace-ale
