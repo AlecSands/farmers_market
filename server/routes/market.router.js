a7d6ae5e3769a8f9b663f110a1e82ac29f5a4c1d
Alessandro Leyva <josealeyva87@gmail.com>
2017-08-03 10:24:35 -0500

    update buy

--- server/routes/market.router.js
+++ server/routes/market.router.js
@@ -6,23 +6,62 @@
 
 // express module
 var express = require('express');
-
-// Express router to mount market related functions on.
-var router = express.Router();
+var user =
+  // Express router to mount market related functions on.
+  var router = express.Router();
 
 // For base mode, use this array
 // As a stretch goal, move this to the database
-var marketItems = [
-  {id: 1, name: 'Apple', cost: 0.99},
-  {id: 2, name: 'Tomatoe', cost: 1.19},
-  {id: 3, name: 'Coffee', cost: 2.99},
-  {id: 4, name: 'Flowers', cost: 8.99},
-  {id: 5, name: 'Orange', cost: 0.89},
-  {id: 6, name: 'Pepper', cost: 1.29},
-  {id: 7, name: 'Lettuce', cost: 2.99},
-  {id: 8, name: 'Basket', cost: 5.99},
-  {id: 9, name: 'Apron', cost: 19.99},
-  {id: 10, name: 'Potholders', cost: 19.99}
+var marketItems = [{
+    id: 1,
+    name: 'Apple',
+    cost: 0.99
+  },
+  {
+    id: 2,
+    name: 'Tomatoe',
+    cost: 1.19
+  },
+  {
+    id: 3,
+    name: 'Coffee',
+    cost: 2.99
+  },
+  {
+    id: 4,
+    name: 'Flowers',
+    cost: 8.99
+  },
+  {
+    id: 5,
+    name: 'Orange',
+    cost: 0.89
+  },
+  {
+    id: 6,
+    name: 'Pepper',
+    cost: 1.29
+  },
+  {
+    id: 7,
+    name: 'Lettuce',
+    cost: 2.99
+  },
+  {
+    id: 8,
+    name: 'Basket',
+    cost: 5.99
+  },
+  {
+    id: 9,
+    name: 'Apron',
+    cost: 19.99
+  },
+  {
+    id: 10,
+    name: 'Potholders',
+    cost: 19.99
+  }
 ];
 
 /**
@@ -31,27 +70,53 @@ var marketItems = [
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
  */
-router.get('/items', function(req, res){
-  console.log('marketRouter - get /items');
-  res.send(marketItems);
-});
 
-router.put('/buy/:id', function(req, res){
-  console.log('marketRouter - put /buy');
-  // TODO: Save to the database
-  res.sendStatus(200); // <- Temporary
-});
+buyItem = function(id) {
+    id = req.params.id;
+    data = req.body;
+    var basket = [];
+    console.log('in update:', id);
+    var price = marketItems[id - 1].cost;
+    if (req.user.money >= price) {
+      user.findByIdAndUpdate({
+          _id: id
+        }, {
+          $set: {
+            basket: data.basket,
+            money: data.money
+          }
+        },
+      }
+      else {
+        res.send('buy stuff');
+      }
+    }
 
-router.put('/sell/:id', function(req, res){
-  console.log('marketRouter - put /sell');
-  // TODO: Save to the database
-  res.sendStatus(200); // <- Temporary
-});
 
-router.get('/leaderboard', function(req, res){
-  console.log('marketRouter - get /leaderboard');
-  // TODO: Retrieve from the database
-  res.send([]); // <- Temporary
-});
 
-module.exports = router;
+
+
+    router.get('/items', function(req, res) {
+      console.log('marketRouter - get /items');
+      res.send(marketItems);
+    });
+
+    router.put('/buy/:id', function(req, res) {
+      console.log('marketRouter - put /buy');
+      // TODO: Save to the database
+      res.sendStatus(200); // <- Temporary
+    });
+
+    router.put('/sell/:id', function(req, res) {
+      console.log('marketRouter - put /sell');
+      // TODO: Save to the database
+      res.sendStatus(200); // <- Temporary
+    });
+
+    router.get('/leaderboard', function(req, res) {
+      console.log('marketRouter - get /leaderboard');
+      // TODO: Retrieve from the database
+      res.send([]); // <- Temporary
+    });
+
+    module.exports = router;
